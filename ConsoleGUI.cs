7e6033a695719dd8ce2a2d1002dde22bb7af5dd9
Samuel Kinnett <samuelkinnett@member.fsf.org>
2015-09-28 19:57:55 +0100

    Added Classes
    Added the basic classes.

---
+++ ConsoleGUI.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+public enum BorderStyles
+{
+    none,
+    oneLine,
+    twoLine,
+}
+
+namespace broadside_client_windows
+{
+    /// <summary>
+    /// This class provides methods to create and manage GUI-like elements in a console environment.
+    /// </summary>
+    public class ConsoleGUI
+    {
+
+        ConsoleRenderer consoleRenderer;
+        List<Element> screen;
+        int numberOfElements;
+        int activeElement;
+
+        ConsoleColor screenColour { get; set; }
+
+        /// <summary>
+        /// Instantiates a new console GUI, creating a consoleRenderer and an array to hold the GUI elements.
+        /// </summary>
+        public ConsoleGUI(ConsoleColor screenColour = ConsoleColor.Black)
+        {
+            consoleRenderer = new ConsoleRenderer(Console.WindowWidth, Console.WindowHeight);
+            screen = new List<Element>(); //This list contains every element to be drawn to the screen. Elements are rendered in the order they're added to the list.
+            numberOfElements = -1;
+            activeElement = -1;
+            this.screenColour = screenColour;
+        }
+
+        /// <summary>
+        /// Draws all the elements to the screen.
+        /// </summary>
+        public void PaintScreen() 
+        {
+            foreach (Element currentElement in screen) {
+                currentElement.DrawElement(consoleRenderer);
+            }
+            consoleRenderer.Paint(screenColour);
+        }
+
+        public string Interact()
+        {
+            return screen[activeElement].Interact();
+        }
+
+        public void Update()
+        {
+            foreach (Element currentElement in screen) {
+                currentElement.Update();
+            }
+        }
+
+        /// <summary>
+        /// Adds an element to the screen
+        /// </summary>
+        public void AddElement(Element newElement, bool makeActive = false)
+        {
+            screen.Add(newElement);
+            numberOfElements++;
+            if (makeActive) {
+                activeElement = numberOfElements++;
+            }
+        }
+
+        /// <summary>
+        /// This class provides the basic features of a GUI element that can then be inherited from.
+        /// </summary>
+        public abstract class Element
+        {
+            public int xPosition;  //The x position of the element
+            public int yPosition;  //The y position of the element
+            public int width;  //The width of the element in characters
+            public int height; //The height of the element in characters
+            public ConsoleColor foreColour; //The foreground colour of the element
+            public ConsoleColor backColour; //The background colour of the element
+            public BorderStyles border;    //The style of border of the element. Borders are always internal.
+
+            public abstract void DrawElement(ConsoleRenderer consoleRenderer);  //Called to draw the element to the buffer.
+            public abstract void DeleteElement();   //Called to remove the element when it is no longer needed.
+            public abstract void Update();  //Called to update the element.
+            public abstract string Interact();  //Called to interact with the element.
+
+            internal Element()
+            {
+                //Used to stop the abstract class being instantiated
+            }
+        }
+
+        /// <summary>
+        /// This class fulfils the same basic purpose as the standard console; it stores lines in a buffer that can then be rendered.
+        /// </summary>
+        public class LogBox : Element
+        {
+            int bufferSize; //The size of the text buffer
+            string[] buffer;    //The contents of the log box.
+            
+            public LogBox(int x, int y, int width, int height, BorderStyles border = BorderStyles.none, ConsoleColor foreColour = ConsoleColor.White, ConsoleColor backColour = ConsoleColor.Black)
+            {
+                this.xPosition = x;
+                this.yPosition = y;
+                this.width = width;
+                this.height = height;
+                this.border = border;
+
+                this.foreColour = foreColour;
+                this.backColour = backColour;
+
+                bufferSize = height;
+                buffer = new string[bufferSize];
+                //fill the buffer with blank strings
+                for (int i = 0; i < bufferSize; i++)
+                    buffer[i] = "";
+            }
+
+            public void WriteLine(string lineToWrite)
+            {
+                string[] newBuffer = new string[bufferSize];    //Create a new buffer into which we will copy our existing buffer
+                Array.Copy(buffer, 0, newBuffer, 1, bufferSize - 1);    //Copy over the contents of the current buffer, cutting off the last entry and leaving the first entry clear.
+                newBuffer[0] = lineToWrite; //Write the new entry to the first element
+                buffer = newBuffer; //Copy the temporary buffer into the buffer variable
+            }
+
+            public override void DrawElement(ConsoleRenderer consoleRenderer) 
+            {
+                if (border == BorderStyles.none) {
+                    //no border, just draw the log.
+                    WriteBuffer(0, consoleRenderer);
+                }
+                else {
+                    //Draw a border before writing the text.
+                    consoleRenderer.DrawBox(xPosition, yPosition, width, height, border, foreColour, backColour);
+                    WriteBuffer(1, consoleRenderer);
+                }
+            }
+
+            private void WriteBuffer(int borderOffset, ConsoleRenderer consoleRenderer)
+            {
+                int tempWidth = width - (borderOffset * 2);
+                int tempHeight = height - (borderOffset * 2);
+                int tempX = xPosition + borderOffset;
+                int tempY = yPosition + borderOffset;
+
+                int linesRemaining = tempHeight;
+                int charsPerLine = tempWidth;
+                for (int currentString = 0; currentString < bufferSize; currentString++) {
+                    if (linesRemaining > 0) {
+                        //For each line, calculate how many lines it will take up in the logbox.
+                        int numberOfLines = (int)Math.Ceiling((double)buffer[currentString].Length / (double)charsPerLine);
+
+                        if (numberOfLines < 2) {
+                            //We only need to write one line; simple!
+                            consoleRenderer.WriteString(tempX, tempY + (linesRemaining - 1), buffer[currentString], foreColour, backColour);
+                            linesRemaining--;
+                        }
+                        else {
+                            //This is where it gets awkward. We need to account for text wrapping.
+                            //To do this we split up the string into several substrings that will fit in the Log Box.
+                            string[] substringArray = new string[numberOfLines];
+                            for (int c = 0; c < numberOfLines - 1; c++) {
+                                substringArray[c] = buffer[currentString].Substring(c * charsPerLine, charsPerLine);
+                            }
+                            substringArray[numberOfLines - 1] = buffer[currentString].Substring((numberOfLines - 1) * charsPerLine);    //The last substring must go to the end of the string.
+
+                            //Now, add each substring to the buffer and update the linesRemaining variable.
+                            for (int c = numberOfLines - 1; c >= 0; c--) {
+                                if (linesRemaining > 0) {
+                                    consoleRenderer.WriteString(tempX, tempY + (linesRemaining - 1), substringArray[c], foreColour, backColour);
+                                    linesRemaining--;
+                                }
+                            }
+                        }
+                    }
+                }
+            }
+
+            public override void DeleteElement()
+            {
+                throw new NotImplementedException();
+            }
+
+            public override string Interact()
+            {
+                //Console.ReadLine();
+                return "";
+            }
+
+            public override void Update()
+            {
+            }
+        }
+    }
+}
---
+++ ConsoleRenderer.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace broadside_client_windows
+{
+    public class ConsoleRenderer
+    {
+        char[] buffer;
+        char[] oldBuffer;   //The buffer rendered previously, used to only draw what is needed.
+        int[] foreColours;  //The foreground colours of the buffer
+        int[] oldForeColours;   //The colours previously rendered
+        int[] backColours;  //The background colours of the buffer
+        int[] oldBackColours;   //The background colours previously rendered
+        int bufferWidth;
+        int bufferHeight;
+        //private StreamWriter stdout;    //Used to write to the console using a stream to get the fastest write speed possible.
+
+        public ConsoleRenderer(int bufferWidth, int bufferHeight)
+        {
+            this.bufferWidth = bufferWidth;
+            this.bufferHeight = bufferHeight;
+            foreColours = new int[bufferWidth * bufferHeight];
+            backColours = new int[bufferWidth * bufferHeight];
+            oldForeColours = new int[bufferWidth * bufferHeight];
+            oldBackColours = new int[bufferWidth * bufferHeight];
+            buffer = new char[bufferWidth * bufferHeight];
+            oldBuffer = new char[bufferWidth * bufferHeight];
+            //stdout = new StreamWriter(Console.OpenStandardOutput(), System.Text.Encoding.Default);
+            //stdout.AutoFlush = false;
+        }
+
+        /// <summary>
+        /// Clears the buffer.
+        /// </summary>
+        private void ClearBuffer(ConsoleColor screenColour)
+        {
+            Array.Copy(buffer, oldBuffer, buffer.Length);
+            Array.Copy(foreColours, oldForeColours, foreColours.Length);
+            Array.Copy(backColours, oldBackColours, backColours.Length);
+            Array.Clear(buffer, 0, buffer.Length);
+            Array.Clear(foreColours, 0, foreColours.Length);
+
+            for (int i = 0; i < backColours.Length; i++) {
+                backColours[i] = (int)screenColour;
+            }
+        }
+
+        /// <summary>
+        /// Outputs the contents of the buffer to the console.
+        /// </summary>
+        private void DrawBuffer()
+        {
+            /*
+            //Concatenate the buffer into one continuous string
+            string output = "";
+            for (int i = 0; i < buffer.Length; i++) {
+                output += buffer[i];
+                //stdout.Write(buffer[i]);
+            }
+            stdout.Write(output);
+            stdout.Flush();
+             * */
+
+            for (int x = 0; x < bufferWidth; x++) {
+                for (int y = 0; y < bufferHeight; y++) {
+                    if (buffer[y * bufferWidth + x] != oldBuffer[y * bufferWidth + x]
+                            || foreColours[y * bufferWidth + x] != oldForeColours[y * bufferWidth + x]
+                            || backColours[y * bufferWidth + x] != oldBackColours[y * bufferWidth + x]) {
+                        Console.SetCursorPosition(x, y);
+                        Console.ForegroundColor = (ConsoleColor)foreColours[y * bufferWidth + x];
+                        Console.BackgroundColor = (ConsoleColor)backColours[y * bufferWidth + x];
+                        Console.Write(buffer[y * bufferWidth + x]);
+                    }
+                }
+            }
+
+        }
+
+        /// <summary>
+        /// Call this to output the buffer to the screen and then clear it.
+        /// </summary>
+        public void Paint(ConsoleColor screenColour)
+        {
+            DrawBuffer();
+            ClearBuffer(screenColour);
+            Console.SetCursorPosition(0, 0);
+        }
+
+        /// <summary>
+        /// Writes a character to the buffer at the specified location.
+        /// </summary>
+        /// <param name="x">The x co-ordinate to write to</param>
+        /// <param name="y">The y co-ordinate to write to</param>
+        /// <param name="c">The character to write</param>
+        private int WriteChar(int x, int y, char c, ConsoleColor foreColour, ConsoleColor backColour)
+        {
+            try {
+                buffer[y * bufferWidth + x] = c;
+                foreColours[y * bufferWidth + x] = (int)foreColour;
+                backColours[y * bufferWidth + x] = (int)backColour;
+                return 0;
+            } catch {
+                return 1;
+            }
+        }
+
+        public void WriteString(int x, int y, string text, ConsoleColor foreColour, ConsoleColor backColour)
+        {
+            char[] characters = text.ToCharArray();
+
+            for (int i = 0; i < characters.Length; i++) {
+                if (WriteChar(x + i, y, characters[i], foreColour, backColour) == 1)
+                    return;
+            }
+        }
+
+        /// <summary>
+        /// Draws a box at the specified location.
+        /// </summary>
+        /// <param name="x"></param>
+        /// <param name="y"></param>
+        /// <param name="width"></param>
+        /// <param name="height"></param>
+        /// <param name="border"></param>
+        public void DrawBox(int x, int y, int width, int height, BorderStyles border, ConsoleColor foreColour, ConsoleColor backColour)
+        {
+            switch (border) {
+                case BorderStyles.oneLine:
+                    //Place corners
+                    WriteChar(x, y, '┌', foreColour, backColour);
+                    WriteChar(x + (width - 1), y, '┐', foreColour, backColour);
+                    WriteChar(x + (width - 1), y + (height - 1), '┘', foreColour, backColour);
+                    WriteChar(x, y + (height - 1), '└', foreColour, backColour);
+                    //Place edges
+                    for (int tempY = y + 1; tempY < y + (height - 1); tempY++) {
+                        WriteChar(x, tempY, '│', foreColour, backColour);
+                        WriteChar(x + (width - 1), tempY, '│', foreColour, backColour);
+                    }
+                    for (int tempX = x + 1; tempX < x + (width - 1); tempX++) {
+                        WriteChar(tempX, y, '─', foreColour, backColour);
+                        WriteChar(tempX, y + (height - 1), '─', foreColour, backColour);
+                    }
+                    break;
+
+                case BorderStyles.twoLine:
+                    //Place corners
+                    WriteChar(x, y, '╔', foreColour, backColour);
+                    WriteChar(x + (width - 1), y, '╗', foreColour, backColour);
+                    WriteChar(x + (width - 1), y + (height - 1), '╝', foreColour, backColour);
+                    WriteChar(x, y + (height - 1), '╚', foreColour, backColour);
+                    //Place edges
+                    for (int tempY = y + 1; tempY < y + (height - 1); tempY++) {
+                        WriteChar(x, tempY, '║', foreColour, backColour);
+                        WriteChar(x + (width - 1), tempY, '║', foreColour, backColour);
+                    }
+                    for (int tempX = x + 1; tempX < x + (width - 1); tempX++) {
+                        WriteChar(tempX, y, '═', foreColour, backColour);
+                        WriteChar(tempX, y + (height - 1), '═', foreColour, backColour);
+                    }
+                    break;
+            }
+        }
+    }
+}
